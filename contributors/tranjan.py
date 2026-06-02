37513ef88006581639c999120b3c384eba6dfc3b
Janey Tran <tranjan@oregonstate.edu>
2026-06-02 10:15:51 -0700

    Add contributor file for tranjan

---
+++ contributors/tranjan.py
@@ -0,0 +1,11 @@
+# Contributor : <Janey Tran>
+# ONID : <tranjan>
+# Term : <Sp26>
+
+def introduce ():
+    print (" Hi, I'm Janey Tran, ONID tranjan")
+    
+if __name__ == " __main__ ":
+    introduce ()
+    
+introduce()
