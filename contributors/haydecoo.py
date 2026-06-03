38f5fcd8624349d13ab7a0187eefa6b7514464cc
haydecoo <haydecoo@oregonstate.edu>
2026-06-03 12:36:48 -0700

    Update haydecoo.py

--- contributors/haydecoo.py
+++ contributors/haydecoo.py
@@ -1,1 +1,8 @@
 
+# Contributor : Cooper Hayden
+# ONID : haydecoo
+# Term : Spring 2026
+def introduce () :
+print (" Hi, I'm Cooper Hayden, ONID haydecoo.")
+if __name__ == " __main__ ":
+introduce ()
